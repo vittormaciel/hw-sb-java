393ac0e866b07499da1d3d2800c9aa18d4c7e203
vittormaciel <contato@vittormaciel.com.br>
2023-05-25 16:13:46 -0300

    Corrigindo Shell Script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # DOWNLOAD DO KUBECTL
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"'
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 # PERMISSÃO PARA O KUBECTL
-chmod +x ./kubectl'
+chmod +x ./kubectl
 
 # EXPORTANDO KUBECONFIG USANDO VARIAVEL CRIADA NO JENKINSFILE
-export KUBECONFIG=$config'
+export KUBECONFIG=$config
 
 # EXECUTANDO DEPLOYMENT FORÇANDO USO DO KUBECONFIG.
-./kubectl apply -f deployment.yaml --kubeconfig=$config'
+./kubectl apply -f deployment.yaml --kubeconfig=$config
