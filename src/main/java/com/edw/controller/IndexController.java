ed28fa94f600f410f88abc136d84d7fd97e54438
vittormaciel <contato@vittormaciel.com.br>
2023-06-06 15:25:33 -0300

    Alterando deploy para canary

--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
         return new HashMap<>() {{
             put("success", true);
-            put("hello", "world");
+            put("hello", "world V2");
             put("new-message", "adding a new msg");
         }};
     }
